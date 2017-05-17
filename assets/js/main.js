528d9f77b00a60927fbea210942f0a977d6bcdac
TomaszGa <tomasz.galina@gmail.com>
2017-05-17 19:51:09 +0200

    refactor javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,29 @@
 
 
 $(document).ready(function(){
-
+	//reference to animation elements and window
 	const $animationElements = $(".animation-element");
 	const $window = $(window);
-	console.log($animationElements);
-	$(window).scroll(function(){
-		$.each($animationElements, function(){
-			if ($(this).visible()){
-				const animationType = $(this).attr("anim-type");
-				pageAnimations.addAnimation($(this), animationType);
-			}
-		});
+
+	//trigger on document load and every window scroll	
+	pageAnimations.checkVisibility($animationElements);
+	$window.scroll(function(){
+		pageAnimations.checkVisibility($animationElements);
 	});
 });
 
 const pageAnimations = {
+	//animate target by modifying classes
 	addAnimation: function(target, anim){
 		$(target).addClass("animated").addClass(anim).removeClass("hidden");		
 	},
-
-/*	checkVisibility: function(animationElements){
-		
-	}*/
+	//check if any animated elments are visible to user
+	checkVisibility: function(animationElements){
+			$.each(animationElements, function(){
+				if ($(this).visible()){
+					const animationType = $(this).attr("anim-type");
+					pageAnimations.addAnimation($(this), animationType);
+			}
+		});
+	}
 }
